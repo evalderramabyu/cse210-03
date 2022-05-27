65d9c68819aed6bb41fbc26fd48e8eee4cfeadbc
Natalia <nparedes517@yahoo.com>
2022-05-26 21:12:37 -0400

    code was updated

--- jumper/game/parachute.py
+++ jumper/game/parachute.py
@@ -1,94 +1,93 @@
 
 class Parachute:
-    def _init_(self):
+
+    def __init__(self):
         """
         Attributes
             _lives: integer
         """
-
-        self.lives = 4 
-        self._level_number = 0
+        self._lives = 4
 
     def lose_life(self):
         """
         Takes a live away from the remaining lives of the game
         """
-        self.lives -= 1
+        self._lives -= 1
 
-    def draw_image(self):
+    def get_lifes(self):
+        """
+        Gets the remaining lives in the game.
+        """
+        return self._lives
+
+    def draw_image(self, terminal_service):
         """Draws a picture of a parachute
         """
-        method = 'level_' + str(self._level_number)
-        return getattr(self, method)()
+        method = '_level_' + str(self._lives)
+        terminal_service.write_text(getattr(self, method)())
 
-    def level_0(self):
+    def _level_4(self):
         """Draws a complete parachute to start the game
         """
         return """
-                ___
-               /___\
-               \   /
-                \ /
-                😀
-                /|\
-                / \  
+            ___
+           /___\\
+           \   /
+            \ /
+             😀
+            /|\\
+            / \\
 
 
 
-           🌿🌿🌿🌿🌿 
-
+        🌿🌿🌿🌿🌿🌿
         """
 
-    def level_1(self):
+    def _level_3(self):
         """Draws a parachute when user looses the first attempt
         """
         return """
-           
-               /___\
-               \   /
-                \ /
-                🤨
-                /|\
-                / \  
+           /___\\
+           \   /
+            \ /
+             🤨
+            /|\\
+            / \\
 
 
-            🌿🌿🌿🌿🌿 
-
+        🌿🌿🌿🌿🌿🌿
         """
 
-    def level_2(self):
+    def _level_2(self):
         """Draws a parachute when user looses the second attempt
-        """        
-        return """               
-               \   /
-                \ /
-                😒
-                /|\
-                / \ 
+        """
+        return """
+           \   /
+            \ /
+             😒
+            /|\\
+            / \\
 
-            🌿🌿🌿🌿🌿 
-
+        🌿🌿🌿🌿🌿🌿
         """
 
-    def level_3(self):
+    def _level_1(self):
         """Draws a parachute when user looses the third attempt.
         """
         return """
-                \ /
-                😧
-                /|\
-                / \ 
-            🌿🌿🌿🌿🌿 
+            \ /
+             😧
+            /|\\
+            / \\
+        🌿🌿🌿🌿🌿🌿
         """
 
-    def level_4(self):
+    def _level_0(self):
         """ Displays the game over picture.
         """
         return """
-                😵
-                /|\
-                / \ 
-            🔥🔥🔥🔥
+             😵
+            /|\\
+            / \\
+        🔥🔥🔥🔥🔥
         """
-
-  
